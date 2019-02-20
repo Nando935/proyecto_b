3e6f02b0ea522494d964d22c300ae3fb6f8f3e6d
Nando935 <fdo+Nando935@users.noreply.github.com>
2019-02-20 08:27:32 -0600

    Algunos cambios
    Cambios recientes

--- index.php
+++ index.php
@@ -6,25 +6,38 @@ while (have_posts()) : the_post(); ?>
 
 <div class="master_container">
 
+<div class="background-header center_elements">
+    <?php echo get_the_post_thumbnail( $page->ID, 'medium_large'); ?>
+</div>
+
 <?php
+    $i=0;
+
+    if($i>4){
+        $i=0;
+    }
+
+    $fondos = array("background-drinks", "background-res", "background-enter", "background-disco", "background-acti");
+
 	$mypages = get_pages( array( 'child_of' => 7, 'sort_column' => 'post_date', 'sort_order' => 'desc' ) ); /*comprobar que el numero de pagina sea el correcto*/
 
 	foreach( $mypages as $page ) {		
 		$content = $page->post_content;
 
     ?>
-        <div class="background-drinks center_elements">
+        <div class="center_elements <?php echo $fondos[$i]; ?>">
         <div class="title_distance"><h1><?php echo $page->post_title; ?></h1></div>
         
         
         <?php
             echo '<div class="textos center_elements">' .$content.'</div>';
             echo get_the_post_thumbnail( $page->ID, 'medium_large');
-		
+            $i++;
+            echo '</div>';
          
 	}	
 ?>
-</div>
+
 </div>
 
 
@@ -33,5 +46,7 @@ endwhile;
 else :
     echo "<p>No hay contenido que mostrar</p>";
    endif;
-get_footer();
 ?>
+ <?php
+    get_footer();
+ ?>
